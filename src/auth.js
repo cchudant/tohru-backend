280257dee4ac7e4bde666fc968e3d53542f81d1a
SkyBeast <skybeastmc@gmail.com>
2018-09-10 23:18:01 +0200

    Remove unnecessary logging

--- src/auth.js
+++ src/auth.js
@@ -8,7 +8,6 @@ module.exports = {
   },
   needAdmin() {
     return (req, res, next) => {
-      console.log(req.user)
       if (req.user && req.user.role && req.user.role === 'admin') return next()
 
       res.status(401).json({ message: 'You are not admin' })
